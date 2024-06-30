84f4759ee3fbb5adc1f1c6b725ecdfa697650d69
Nikkipandey566 <pandeynikki250@gmail.com>
2024-06-30 15:21:34 +0530

    Add files via upload

---
+++ ant.py
@@ -0,0 +1,38 @@
+# swaping fo 2 variables:-
+#a=int(input("enter value of a:"))
+#b=int(input("enter value of b:"))
+#t=a
+#a=b
+#b=t
+#print("after swaping")
+#print("a=",a)
+#print("b=",b)
+#-------------------------------
+ #-sum,sub,mal,quo,rum:-
+#a=float(input("enter value of a: "))
+#b=float(input("enyer value of b: "))
+#sum=a+b
+#sub=a-b
+#mal=a*b
+#quo=a/b
+#rum=a%b
+#print("sum=",sum)
+#print("sub=",sub)
+#print("mal=",mal)
+#print("quo=",quo)
+#print("rum=",rum)
+#----------------------------------
+#-area and perimeter of rectamgle
+#l=int(input("enter value of l:"))
+#b=int(input("enyer value of B:"))
+#a=l*b
+#p=2*l*b
+#print("a=",a)
+#print("p=",p)
+#-----------------------------------
+#p=float(input("enter value of p:"))
+#t=float(input("enter value of t:"))
+#r=float(input("enter value of r:"))
+#si=(p*t*r)/100
+#print("si=",si)
+
